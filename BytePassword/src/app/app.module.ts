671c723bd3b96c76954de1f7c35fb1185c578328
Rurangwa <chrisruu@yahoo.com>
2023-11-28 14:05:03 +0800

    add material

--- BytePassword/src/app/app.module.ts
+++ BytePassword/src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import {MatCardModule} from '@angular/material/card'
+import { MatDialogModule} from '@angular/material/dialog'
 
 import { AppComponent } from './app.component';
 import { BytePwdComponent } from './components/byte-pwd/byte-pwd.component';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -13,9 +16,13 @@ import { RouterModule } from '@angular/router';
     BytePwdComponent
   ],
   imports: [
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatDialogModule,
     BrowserModule,HttpClientModule,FormsModule,
     RouterModule.forRoot([{path:'byte-pwd',component: BytePwdComponent},
-  ])
+  ]),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
