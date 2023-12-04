d8169cf0e7b5060e6974fc41b2296c0c598fb6e3
Rurangwa <chrisruu@yahoo.com>
2023-12-04 11:52:04 +0800

    updates on model class

---
+++ BytePassword/src/app/models/CredentialTemplate.ts
@@ -0,0 +1,14 @@
+export interface CredentialTemplate{
+
+   password:string;
+   email:string;
+    name:string;
+   hint:string;
+  message:string;
+   logo:string;
+  timestamp:string;
+    action:string;
+   original:string;
+
+
+}
